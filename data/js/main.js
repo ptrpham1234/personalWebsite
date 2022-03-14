9707a9c6954b1c320b5678f94bc3d02dbf61973e
Peter Pham <ptrpham2468@gmail.com>
2022-03-14 10:30:44 -0500

    added the start of the explore animation

--- data/js/main.js
+++ data/js/main.js
@@ -24,3 +24,46 @@ function copyOutFunc() {
     var tooltip = document.getElementById("email-tooltip")
     tooltip.innerHTML = "Copy to clipboard";
 }
+
+function fadeOut() {
+    TweenMax.to(".explore-button", 1, {
+        y: -20,
+        opacity: 0,
+    });
+
+    TweenMax.to(".card", 2, {
+        y: -400,
+        opacity: 0,
+        ease: Power2.easeInOut,
+        delay: 2,
+    });
+
+    TweenMax.from(".wrapper", 2, {
+        ease: Power2.easeInOut,
+    });
+
+    TweenMax.to(".wrapper", 2, {
+        delay: 2.6,
+        top: "-110%",
+        ease: Expo.easeInOut,
+    });
+
+    TweenMax.to(".overlay", 2, {
+        delay: 3,
+        top: "-110%",
+        ease: Expo.easeInOut,
+    });
+
+    TweenMax.from(".content", 2, {
+        delay: 3.2,
+        opacity: 0,
+        ease: Power2.easeInOut,
+    });
+
+    TweenMax.to(".content", 2, {
+        opacity: 1,
+        y: -300,
+        delay: 3.2,
+        ease: Power2.easeInOut,
+    });
+}
